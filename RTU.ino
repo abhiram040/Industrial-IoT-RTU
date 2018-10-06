a1fb4a4ac400a175f812856ff4b0420fb405f905
abhiram040 <37603626+abhiram040@users.noreply.github.com>
2018-10-06 19:37:51 +0530

    Add files via upload

---
+++ RTU.ino
@@ -0,0 +1,37 @@
+#include <dht.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+dht DHT;
+ 
+void setup(){
+  pinMode(8,OUTPUT);
+  digitalWrite(8,HIGH);
+  Serial.begin(9600);
+  sensors.begin(); 
+  delay(500);//Delay to let system boot
+  
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop(){
+  //Start of Program 
+   sensors.requestTemperatures(); 
+    DHT.read11(A1);
+    
+    Serial.print(sensors.getTempCByIndex(0));
+    Serial.print(DHT.humidity);
+    Serial.print("% ");
+    Serial.print(":");
+    Serial.print(DHT.temperature); 
+    Serial.println("C ");
+    
+    delay(2000);//Wait 5 seconds before accessing sensor again.
+ 
+  //Fastest should be once every two seconds.
+ 
+}// end loop() 
